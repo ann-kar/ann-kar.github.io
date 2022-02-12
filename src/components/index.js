60a1510341180febeceb2a1ea21bb0ea466e60b4
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-02-12 21:20:54 +0100

    added barrels

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Header from '../components/Header';
+import HeaderList from '../components/HeaderList';
+import Main from '../components/Main';
+import Projects from '../components/Projects';
+
+export {Header, HeaderList, Main, Projects}
