6eacd2a94c384620ae151807429938d297e14af0
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-02-12 21:34:12 +0100

    added project data

---
+++ src/data/project-data.js
@@ -0,0 +1,36 @@
+const projects = [
+    {
+        name: 'Set',
+        technologies: ['TypeScript', 'React', 'SASS'],
+        description: 'My online version of the SET card game, following the original logic & timed, with cards dynamically generated from a predefined set of symbols. The older version available here',
+        demo: 'https://ann-kar.github.io/set/',
+        code: 'https://github.com/ann-kar/set',
+        label: 'last project'
+    },
+    {
+        name: 'Shift planner',
+        technologies: ['Express.js', 'EJS', 'PostgreSQL', 'Bootstrap'],
+        description: 'A coffeeshop employee scheduling app built with Node.js as an exercise in server-side JS, fetching from the database, and data presentation (calendar on the homepage).',
+        demo: null,
+        code: 'https://github.com/ann-kar/shift-planner',
+        label: 'recent project'
+    },
+    {
+        name: 'Calculator',
+        technologies: ['JavaScript (ES6)', 'Jest', 'webpack', 'babel', 'ESLint'],
+        description: 'A JavaScript app handling basic calculator functions (addition, subtraction, multiplication, division), complete with ten test suites written in Jest.',
+        demo: null,
+        code: 'https://github.com/ann-kar/calculator',
+        label: 'recent project'
+    },
+    {
+        name: 'Accordion FAQ',
+        technologies: ['TypeScript', 'React', 'styled-components'],
+        description: 'A coffeeshop employee scheduling app built with Node.js as an exercise in server-side JS, fetching from the database, and data presentation (calendar on the homepage).',
+        demo: 'https://ann-kar.github.io/accordion-faq/',
+        code: 'https://github.com/ann-kar/accordion-faq/',
+        label: 'small project'
+    },
+];
+
+export default projects;
