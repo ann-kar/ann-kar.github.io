026de1611b45c4c8ebe02aef237df3ed60c3a727
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-02-14 15:49:29 +0100

    added Message component

---
+++ src/components/Message.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Typography from '@mui/material/Typography';
+
+const Message = () => {
+    return (
+        <Typography sx={{ my: 2 }} variant="h2" component="h2" color="rgba(255,255,255,0.8)" textAlign="left">
+            check out my recent projects:
+        </Typography>
+    )
+}
+
+export default Message
--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,13 @@
+import About from '../components/About';
+import Background from '../components/Background';
 import Header from '../components/Header';
 import HeaderList from '../components/HeaderList';
 import LastProject from '../components/LastProject';
 import Main from '../components/Main';
+import Message from '../components/Message';
 import Project from '../components/Project';
 import Projects from '../components/Projects';
+import Skills from '../components/Skills';
+import SkillSet from './SkillSet';
 
-export {Header, HeaderList, LastProject, Main, Project, Projects}
+export {About, Background, Footer, Header, HeaderList, LastProject, Main, Message, Project, Projects, Skills, SkillSet}
