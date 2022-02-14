b1a686ace3b23c344c6c1a37f3cf458f1d20858d
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-02-14 15:48:09 +0100

    updated margins

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,12 +5,13 @@ import styled from '@mui/system/styled';
 import {HeaderList} from '../components';
 
 const HeaderWrapper = styled('header')(({ theme }) => ({
-    color: theme.palette.greys.grey200
+    color: theme.palette.greys.grey200,
+    maxWidth: '100vw',
 }));
 
 const Header = () => {
     return (
-        <HeaderWrapper sx={{ mx: 2, py: 5 }}>
+        <HeaderWrapper sx={{ pl: 2, py: {xs: 3, md: 5} }}>
             <Typography variant="h1">
                 anna karpiuk
             </Typography>
