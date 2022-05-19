2d192c8675708ec9cedae55b20d3acb528a4ee9d
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-05-19 03:06:40 +0200

    add 3 new projects

--- src/data/project-data.js
+++ src/data/project-data.js
@@ -1,37 +1,74 @@
 const lastProject = {
-    name: 'Set',
-    technologies: ['TypeScript', 'React', 'SASS', 'SVG'],
-    description: 'My online version of the SET card game, following the original logic & timed, with cards dynamically generated from a predefined set of symbols.',
-    demo: 'https://ann-kar.github.io/set/',
-    code: 'https://github.com/ann-kar/set',
-    label: 'last project'
-}
+  name: "Set",
+  technologies: ["TypeScript", "React", "SASS", "SVG"],
+  description:
+    "My online version of the SET card game, following the original logic & timed, with cards dynamically generated from a predefined set of symbols.",
+  demo: "https://ann-kar.github.io/set/",
+  code: "https://github.com/ann-kar/set",
+  label: "last project",
+};
 
 const projects = [
-    {
-        name: 'Shift planner',
-        technologies: ['Node.js (Express)', 'EJS', 'PostgreSQL', 'Bootstrap'],
-        description: 'An employee scheduling app built with Node.js as an exercise in server-side JS, fetching from the database, and data presentation (calendar on the homepage).',
-        demo: null,
-        code: 'https://github.com/ann-kar/shift-planner',
-        label: 'recent project'
-    },
-    {
-        name: 'Calculator',
-        technologies: ['JavaScript (ES6)', 'Jest', 'webpack', 'babel', 'ESLint'],
-        description: 'A JavaScript app handling basic calculator functions (addition, subtraction, multiplication, division), complete with ten test suites written in Jest.',
-        demo: null,
-        code: 'https://github.com/ann-kar/calculator',
-        label: 'recent project'
-    },
-    {
-        name: 'Accordion FAQ',
-        technologies: ['TypeScript', 'React', 'styled-components'],
-        description: 'A small project intended as an exercise in styled-components and TypeScript, based on a Figma design. A responsive landing page displaying FAQ and an SVG graphic.',
-        demo: 'https://ann-kar.github.io/accordion-faq/',
-        code: 'https://github.com/ann-kar/accordion-faq/',
-        label: 'small project'
-    },
+  {
+    name: "Bike, run, swim",
+    technologies: [
+      "TypeScript",
+      "Next.js",
+      "react-hook-form",
+      "class-validator",
+      "Tailwind",
+    ],
+    description:
+      "A training app for triathlonists. Allows to add workouts, display them as a list, and edit data.",
+    demo: "bike-run-swim.herokuapp.com",
+    code: "github.com/ann-kar/bikerunswim",
+    label: "recent project",
+  },
+  {
+    name: "Game of life",
+    technologies: ["TypeScript", "Next.js", "Jest (TDD)", "npm", 'Tailwind'],
+    description:
+      "Visualization of Conway's Game of Life algorithm. A pair project using monorepo.",
+    demo: "game-of-life-vis.herokuapp.com",
+    code: "github.com/ann-kar/monorepo-game-of-life",
+    label: "recent project",
+  },
+  {
+    name: "Rick and Morty",
+    technologies: ["TypeScript", "Next.js", "react-query", "Tailwind"],
+    description:
+      "A tribute to the Rick and Morty show - an app for searching specific characters by name or other parameters.",
+    demo: "rick-morty-dun.vercel.app",
+    code: "https://github.com/ann-kar/rick-morty",
+    label: "recent project",
+  },
+  {
+    name: "Shift planner",
+    technologies: ["Node.js (Express)", "EJS", "PostgreSQL", "Bootstrap"],
+    description:
+      "An employee scheduling app built with Node.js as an exercise in server-side JS, fetching from the database, and data presentation (calendar on the homepage).",
+    demo: null,
+    code: "https://github.com/ann-kar/shift-planner",
+    label: "older project",
+  },
+  {
+    name: "Calculator",
+    technologies: ["JavaScript (ES6)", "Jest", "webpack", "babel", "ESLint"],
+    description:
+      "A JavaScript app handling basic calculator functions (addition, subtraction, multiplication, division), complete with ten test suites written in Jest.",
+    demo: null,
+    code: "https://github.com/ann-kar/calculator",
+    label: "older project",
+  },
+  {
+    name: "Accordion FAQ",
+    technologies: ["TypeScript", "React", "styled-components"],
+    description:
+      "A small project intended as an exercise in styled-components and TypeScript, based on a Figma design. A responsive landing page displaying FAQ and an SVG graphic.",
+    demo: "https://ann-kar.github.io/accordion-faq/",
+    code: "https://github.com/ann-kar/accordion-faq/",
+    label: "small project",
+  },
 ];
 
-export {lastProject, projects};
+export { lastProject, projects };
