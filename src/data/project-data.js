6eda83b6f111fb1804c379075bd40fb1f2e5197a
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-06-07 00:38:21 +0200

    update order

--- src/data/project-data.js
+++ src/data/project-data.js
@@ -11,6 +11,27 @@ const lastProject = {
 const projects = [
   {
     id: "1",
+    name: "Rick and Morty",
+    technologies: ["TypeScript", "Next.js", "react-query", "Tailwind"],
+    description:
+      "A tribute to the Rick and Morty show - an app for searching specific characters by name or other parameters.",
+    demo: "https://rick-morty-dun.vercel.app",
+    code: "https://github.com/ann-kar/rick-morty",
+    label: "recent project",
+  },
+  {
+    id: "2",
+    name: "Game of life",
+    technologies: ["TypeScript", "Next.js", "Jest (TDD)", "npm", "Tailwind"],
+    description:
+      "Visualization of Conway's Game of Life algorithm. A pair project using monorepo.",
+    demo: "https://game-of-life-vis.herokuapp.com",
+    code: "https://github.com/ann-kar/monorepo-game-of-life",
+    label: "recent project",
+  },
+
+  {
+    id: "3",
     name: "Bike, run, swim",
     technologies: [
       "TypeScript",
@@ -26,27 +47,17 @@ const projects = [
     label: "recent project",
   },
   {
-    id: "2",
-    name: "Game of life",
-    technologies: ["TypeScript", "Next.js", "Jest (TDD)", "npm", "Tailwind"],
-    description:
-      "Visualization of Conway's Game of Life algorithm. A pair project using monorepo.",
-    demo: "https://game-of-life-vis.herokuapp.com",
-    code: "https://github.com/ann-kar/monorepo-game-of-life",
-    label: "recent project",
-  },
-  {
-    id: "3",
-    name: "Rick and Morty",
-    technologies: ["TypeScript", "Next.js", "react-query", "Tailwind"],
-    description:
-      "A tribute to the Rick and Morty show - an app for searching specific characters by name or other parameters.",
-    demo: "https://rick-morty-dun.vercel.app",
-    code: "https://github.com/ann-kar/rick-morty",
-    label: "recent project",
-  },
-  {
     id: "4",
+    name: "Accordion FAQ",
+    technologies: ["TypeScript", "React", "styled-components"],
+    description:
+      "A small project intended as an exercise in styled-components and TypeScript, based on a Figma design. A responsive landing page displaying FAQ and an SVG graphic.",
+    demo: "https://ann-kar.github.io/accordion-faq/",
+    code: "https://github.com/ann-kar/accordion-faq/",
+    label: "small project",
+  },
+  {
+    id: "5",
     name: "Shift planner",
     technologies: ["Node.js (Express)", "EJS", "PostgreSQL", "Bootstrap"],
     description:
@@ -56,7 +67,7 @@ const projects = [
     label: "older project",
   },
   {
-    id: "5",
+    id: "6",
     name: "Calculator",
     technologies: ["JavaScript (ES6)", "Jest", "webpack", "babel", "ESLint"],
     description:
@@ -65,16 +76,7 @@ const projects = [
     code: "https://github.com/ann-kar/calculator",
     label: "older project",
   },
-  {
-    id: "6",
-    name: "Accordion FAQ",
-    technologies: ["TypeScript", "React", "styled-components"],
-    description:
-      "A small project intended as an exercise in styled-components and TypeScript, based on a Figma design. A responsive landing page displaying FAQ and an SVG graphic.",
-    demo: "https://ann-kar.github.io/accordion-faq/",
-    code: "https://github.com/ann-kar/accordion-faq/",
-    label: "small project",
-  },
+
 ];
 
 export { lastProject, projects };
